9bfa796f65109dea7b3ab5a0a2408f4bc666ab47
madhanmohans <madhanmohan.rms@gmail.com>
2022-11-16 10:10:00 +0530

    updated sprint 1 source code

--- Project Development Phase/Sprint 1/flaskr/__init__.py
+++ Project Development Phase/Sprint 1/flaskr/__init__.py
@@ -1,23 +1,45 @@
-import os
+from dotenv import dotenv_values
 from flask import Flask
+from flask_cors import CORS
+import ibm_db
 
-import flaskr.views
+# Get the environment variables
+config = dotenv_values("backend/.env")
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+# Connect to db
+try:
+    # conn = 'dd'
+    conn = ibm_db.pconnect(
+        f"DATABASE={config['DB2_DATABASE']};HOSTNAME={config['DB2_HOSTNAME']};PORT={config['DB2_PORT']};SECURITY=SSL; SSLServerCertificate=backend/DigiCertGlobalRootCA.crt;UID={config['DB2_USERNAME']};PWD={config['DB2_PASSWORD']}", '', '')
+    print("Connected to IBM_DB2 successfully!!")
+    print(conn)
+except:
+    print("Failed to connect to Database!")
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+def create_app():
+    # Tell flask to use the build directory of react to serve static content
+    app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+    CORS(app)
+
+    # Set the secret key for flask
+    app.config['SECRET_KEY'] = config['APP_SECRET']
+
+    # Import and register auth_router
+    from .auth_router import auth
+    app.register_blueprint(auth, url_prefix='/api/auth')
+
+    from .files_router import files
+    app.register_blueprint(files, url_prefix='/api/files')
+
+    from .user_router import user
+    app.register_blueprint(user, url_prefix='/api/user')
+
+    # In production serve the index.html page at root
+
+    @app.route("/")
+    def home():
+        return app.send_static_file('index.html')
 
     return app
--- Project Development Phase/Sprint 1/flaskr/views.py
+++
@@ -1,5 +0,0 @@
-from flaskr import app
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
